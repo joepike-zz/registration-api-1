5d88e0b60e8a80b20f7276e5b14d68b5b009485a
Pedro Miguel <pedro.miguel@live.co.uk>
2018-10-02 20:35:49 +0100

    Add metadata naming conventions

--- src/extensions.py
+++ src/extensions.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from sqlalchemy import MetaData
 """
 Extensions module.
 
@@ -9,5 +10,14 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
 migrate = Migrate()
